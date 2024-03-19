6f2f3b5629be03a8334d5afb674c5fc263e4fb58
ednakiruai <ednakiruai@gmail.com>
2024-03-19 11:51:16 +0300

    completion

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const multiply = 62
+const num1 = 31
+const num2 = 2
+
+let random = 0;
+random++
+console.log(random)
+
+const mod = 4
+const num3 = 24
+const num4 = 10
+
+const max = 20
+
+
+
+
